0a46974876e6b79e760323fab4ecf46b12553db2
kalapee <35555664+kalapee@users.noreply.github.com>
2024-04-29 12:34:15 +0000

    variables

---
+++ first/Variables.js
@@ -0,0 +1,10 @@
+const accountId = 444222
+let accountEmail = "kd@gmail.com"
+var accountPassword = "484848"
+
+
+accountEmail = "jeny@gmail.com"
+accountPassword = "242424"
+accountCity = "Gujrat"
+
+console.table([accountId, accountEmail, accountPassword])
