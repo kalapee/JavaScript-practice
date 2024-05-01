f76424947e43116c755149d97099e609b9179418
kalapee <35555664+kalapee@users.noreply.github.com>
2024-05-01 17:33:26 +0000

    Commonly Used Comparison Operators

---
+++ first/comparison.js
@@ -0,0 +1,13 @@
+const a = 5, b = 8;
+console.log(a > b);
+console.log(a < b);
+// JavaScript Equal To Operator //
+console.log(5 == 5);
+console.log(5 == "5");
+console.log("hello" == "Helllo");
+// Not Equal To Operator//
+console.log(2 != "2");
+console.log(2 != 3);
+// Strict Equal To Operator //
+console.log(5 === 5);
+console.log(5 === "5");
