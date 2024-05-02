2b677cb6bb9495421d9ff3c54929f8469a258f6f
kalapee <35555664+kalapee@users.noreply.github.com>
2024-05-02 16:27:48 +0000

    Logical AND Operator

--- first/comparison.js
+++ first/comparison.js
@@ -11,3 +11,15 @@ console.log(2 != 3);
 // Strict Equal To Operator //
 console.log(5 === 5);
 console.log(5 === "5");
+// Logical AND Operator //
+
+let x = 2;
+
+// both expressions are true //
+console.log((x < 4) && (4 >= x));  
+
+// only one expression is true //
+console.log((x <= 4) && (2 == 4));  
+
+// both expressions are false //
+console.log((x > 4) && (x == 4));  
