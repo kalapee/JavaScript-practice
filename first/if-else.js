ff907ccb4077c3fe3ad408e59bbf51b8a587d40a
kalapee <35555664+kalapee@users.noreply.github.com>
2024-05-03 14:54:56 +0000

    else and if

---
+++ first/if-else.js
@@ -0,0 +1,25 @@
+// Program to check if the number is positive
+
+const number = ("Enter a number: ");
+
+// check if number is greater than 0
+if (number > 0) {
+    // the body of the if statement
+    console.log("positive number");
+}
+
+console.log("nice number");
+
+let age = 19;
+
+// if age is 18 or above, you are an adult
+// otherwise, you are a minor
+
+if (age >= 18) {
+    console.log("You are an adult");
+}
+else {
+    console.log("You are a minor");
+}
+
+// Output: You are a minor
