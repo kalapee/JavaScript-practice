dd8fea367e81ffb30d56d88de22fa8e96142b158
kalapee <35555664+kalapee@users.noreply.github.com>
2024-05-04 15:48:06 +0000

    A function is an independent block of code that performs a specific task, while a function expression is a way to store functions in variables.

---
+++ first/function.js
@@ -0,0 +1,32 @@
+function good(name) {
+    console.log(`Hello ${name}`);
+}
+
+good("Sati");
+
+good("Dev");
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    console.log(`Sum: ${sum}`);
+}
+
+addNumbers(8, 6);
+
+function findSquare(num) {
+    return num * num;   
+}
+
+let square = findSquare(6);
+
+console.log(`Square: ${square}`);
+
+let band = "variable to uppercase, resulting in";
+let bandUpper = band.toUpperCase();
+console.log(`Favorite Band: ${bandUpper}`);
+
+let squared = function(num) {
+    return num * num;
+}
+
+console.log(squared(50));
---
+++ first/loops.js
@@ -0,0 +1,22 @@
+for (let i = 0; i < 4; i++) {
+    console.log("JavaScript for loop");
+}
+
+for (let i = 0; i < 8; i++) {
+    console.log(i);
+}
+
+let sum = 0;
+const n = 80
+
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+
+console.log(`sum: ${sum}`);
+
+const fruits = ["apple", "banana", "cherry", "Mango"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
